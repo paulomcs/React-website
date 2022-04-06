635531473e6726fae3cdfd2c869343df84f4c976
paulomcs <52682759+paulomcs@users.noreply.github.com>
2022-04-06 17:54:25 -0300

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Footer(){
-    return(
-        <footer className="footer"><small>© 2022 Orlua development. All rights reserved</small></footer>
-    )
-}
