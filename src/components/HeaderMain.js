1817d28a3c99c2832cf887e59a8b25f556237a4c
paulomcs <52682759+paulomcs@users.noreply.github.com>
2022-04-06 22:09:39 -0300

    Add files via upload

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -1,6 +1,5 @@
 import React from "react"
 import {Navbar, Nav, Container, Form, FormControl, Button} from "react-bootstrap"
-import { Link } from "react-router-dom";
 
 export default function Header(){
   return(
